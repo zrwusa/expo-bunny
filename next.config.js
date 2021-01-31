403c612074621dbc5d511babad64d6304a8c0ec6
zrwusa <zrwusa@gmail.com>
2021-01-31 16:26:44 +0800

    try next.js offline

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const { withExpo } = require('@expo/next-adapter')
 const withFonts = require('next-fonts')
 const withImages = require('next-images')
 const withPlugins = require('next-compose-plugins')
-// const withOffline = require('next-offline')
+const withOffline = require('next-offline')
 
 const withTM = require('next-transpile-modules')([
     'expo-next-react-navigation',
@@ -11,8 +11,33 @@ const withTM = require('next-transpile-modules')([
 ])
 
 module.exports = withPlugins(
-    [withTM, withFonts, withImages, [withExpo, { projectRoot: __dirname }]],
+    [withTM, withFonts, withImages,withOffline, [withExpo, { projectRoot: __dirname }]],
     {
+        target: 'serverless',
+        transformManifest: manifest => ['/'].concat(manifest), // add the homepage to the cache
+        // Trying to set NODE_ENV=production when running yarn dev causes a build-time error so we
+        // turn on the SW in dev mode so that we can actually test it
+        generateInDevMode: true,
+        workboxOpts: {
+            swDest: 'static/service-worker.js',
+            runtimeCaching: [
+                {
+                    urlPattern: /^https?.*/,
+                    handler: 'NetworkFirst',
+                    options: {
+                        cacheName: 'https-calls',
+                        networkTimeoutSeconds: 15,
+                        expiration: {
+                            maxEntries: 150,
+                            maxAgeSeconds: 30 * 24 * 60 * 60, // 1 month
+                        },
+                        cacheableResponse: {
+                            statuses: [0, 200],
+                        },
+                    },
+                },
+            ],
+        },
         async rewrites() {
             return [
                 // Rewrite everything else to use `pages/index`
