e067ce7e59264ccfb7363a5b929acbecebe6605d
zrwusa <zrwusa@gmail.com>
2021-02-06 15:32:10 +0800

    styles optimization

--- src/screens/DemoNotification/styles.ts
+++ src/screens/DemoNotification/styles.ts
@@ -1,33 +1,36 @@
 import {StyleSheet} from "react-native";
-import {responsiveFromUE} from "../../styles/utils/responsive";
-import {ms} from "../../styles/utils";
 import {pl} from "../../styles/utils";
+import {Measure, Responsive} from "../../types/styles";
 
-const {wp, hp} = responsiveFromUE.iphoneX;
-const styles = StyleSheet.create({
-    logo: {
-        width: wp(305),
-        height: wp(159),
-        marginBottom: ms.sp.l,
-    },
-    instructions: {
-        color: '#888',
-        fontSize: ms.fs.m,
-        marginHorizontal: ms.sp.l,
-        marginBottom: ms.sp.m,
-    },
-    button: {
-        padding: ms.sp.l,
-        borderRadius: ms.br.xs,
-    },
-    buttonText: {
-        fontSize: ms.fs.l,
-        color: pl.white,
-    },
-    thumbnail: {
-        width: wp(300),
-        height: wp(300),
-        resizeMode: 'contain',
-    },
-});
-export default styles;
+const getStyles = (measure:Measure,responsive:Responsive)=>{
+    const {wp, hp} = responsive.iphoneX;
+    const ms = measure;
+    return  StyleSheet.create({
+        logo: {
+            width: wp(305),
+            height: wp(159),
+            marginBottom: ms.sp.l,
+        },
+        instructions: {
+            color: '#888',
+            fontSize: ms.fs.m,
+            marginHorizontal: ms.sp.l,
+            marginBottom: ms.sp.m,
+        },
+        button: {
+            padding: ms.sp.l,
+            borderRadius: ms.br.xs,
+        },
+        buttonText: {
+            fontSize: ms.fs.l,
+            color: pl.white,
+        },
+        thumbnail: {
+            width: wp(300),
+            height: wp(300),
+            resizeMode: 'contain',
+        },
+    });
+}
+
+export default getStyles;
