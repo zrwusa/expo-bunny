058c290bd6ace33b4e293c87fe9a55c57882bc12
zrwusa <zrwusa@gmail.com>
2021-04-24 10:06:09 +0800

    remove duplicate env key

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,5 +1,5 @@
 import firebase from 'firebase';
-import {FIREBASE_API_KEY, FIREBASE_APP_ID, FIREBASE_AUTH_DOMAIN, FIREBASE_DATABASE_URL} from "@env";
+import {GOOGLE_API_KEY, FIREBASE_APP_ID, FIREBASE_AUTH_DOMAIN, FIREBASE_DATABASE_URL} from "@env";
 // Optionally import the services that you want to use
 //import "firebase/auth";
 //import "firebase/database";
@@ -8,7 +8,7 @@ import {FIREBASE_API_KEY, FIREBASE_APP_ID, FIREBASE_AUTH_DOMAIN, FIREBASE_DATABA
 //import "firebase/storage";
 
 export const FIREBASE_CONFIG = {
-    apiKey: FIREBASE_API_KEY,
+    apiKey: GOOGLE_API_KEY,
     authDomain: FIREBASE_AUTH_DOMAIN,
     databaseURL: FIREBASE_DATABASE_URL,
     projectId: 'expo-react-bunny',
