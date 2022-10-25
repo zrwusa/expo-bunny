dd1047d27da707648c647913cf4f38ef13b35529
Revone <zrwusa@gmail.com>
2022-10-25 10:43:55 +0800

    code optimization

--- src/screens/ColorFinder/styles.ts
+++ src/screens/ColorFinder/styles.ts
@@ -1,12 +1,10 @@
 import {StyleSheet} from 'react-native';
 import {SizeLabor, ThemeLabor} from '../../types';
-import {getSharedStyles} from '../../helpers';
 
 export const makeStyles = (sizeLabor: SizeLabor, themeLabor: ThemeLabor) => {
     const {ms, designsBasedOn} = sizeLabor;
-    const {wp, hp} = designsBasedOn.iphoneX;
+    const {wp} = designsBasedOn.iphoneX;
     const {colors} = themeLabor.theme;
-    const {sharedStylesFlatten} = getSharedStyles(sizeLabor, themeLabor);
 
     return StyleSheet.create({
         container: {
